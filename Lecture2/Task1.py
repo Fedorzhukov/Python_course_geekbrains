eeb97bc87bd138e5aad473074c74c5f3a86c3391
FedorZhukov <zhukov.fedor@icloud.com>
2020-03-29 15:43:52 +0300

    delete

--- Lecture2/Task1.py
+++
@@ -1,3 +0,0 @@
-list_one = ['123', 456, 0.5, 'one', [1, 2, '3']]
-for i in list_one:
-    print(type(i))
--- Lecture2/Task4.py
+++
@@ -1,8 +0,0 @@
-line = 'Привет! Меня зовут Иван Иванов. Приятно познакомиться!'
-new_line = line.split(' ')
-n = 0
-for i in (new_line):
-    if len(i) > 10:
-        i = i[:10]
-    print(n, i)
-    n += 1
