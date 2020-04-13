5553854ae9d89c8dd9e83556a38c3b2cbdd739fc
FedorZhukov <zhukov.fedor@icloud.com>
2020-04-13 15:31:03 +0300

    Update Task1.py

--- L6/Task1.py
+++ L6/Task1.py
@@ -17,22 +17,5 @@ example = TrafficLight()
 example.running()
 
 
-# Не понял как условнять. Что значит проверка порядка режимов, если они всегда в одном порядке. Или пользователь сам
+# Не понял как усложнять. Что значит проверка порядка режимов, если они всегда в одном порядке. Или пользователь сам
 # выбирает какой режим включить?
-
-
-# class TrafficLight:
-#     __color = ['green', 'yellow', 'red']
-#     __waiting_time = {'green': 5, 'yellow': 2, 'red': 7}
-#
-#     def running(self):
-#         while True:
-#             cur_color = input('1-Green, 2-Yellow, 3-Red. Enter-quit')
-#             if cur_color == '':
-#                 break
-#             print(self.__waiting_time.keys())
-#
-#
-#
-# example = TrafficLight()
-# example.running()
